ee4c38f9d4688aa1b07810fa1236e7838a37b569
user <user@WIN-MHGL1PRC8MH>
2024-06-21 12:22:18 +0300

    1

--- ConsoleApplication2/ConsoleApplication1.cpp
+++ ConsoleApplication2/ConsoleApplication1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 
 class ErrorA2 {
-    string str = "DevideByZero";
+    string str = "A2";
 public:
     ErrorA2(string s) : str(s) { }
     void Message()
@@ -18,7 +18,7 @@ public:
     }
 };
 class ErrorA3 {
-    string str = "OpenFile";
+    string str = "A3";
 public:
     ErrorA3(string s) : str(s) { }
     void Message()
@@ -26,41 +26,15 @@ public:
         cout << "Error: " << str << endl;
     }
 };
-
-
-double Gold(double x, double y) throw (ErrorA2, double) {
-    double term = 0;
-    if (x > y && y != 0) {
-        term = x / y;
+class ErrorA4 {
+    string str = "A4";
+public:
+    ErrorA4(string s) : str(s) { }
+    void Message()
+    {
+        cout << "Error: " << str << endl;
     }
-    else if (y > x && x != 0) {
-        term = y / x;
-    }
-    else {
-        throw ErrorA2("DevideByZero");
-    }
-
-    return term;
-}
-
-double Glr(double x, double y) throw (ErrorA2, double) {
-    double term = 0;
-    if (x < 1 || (x * -1) < 1) {
-        term = x;
-    }
-    if ((x >= 1 || (x * -1) >= 1) && (y < 1 || (y * -1) < 1)) {
-        term = y;
-    }
-    if ((x >= 1 || (x * -1) >= 1) && (y >= 1 || (y * -1) >= 1) && (sqrt(x * x + y * y - 4) > 0.1)) {
-        term = y / sqrt(x * x + y * y - 4);
-    }
-    else {
-        throw ErrorA2("Sqrt");
-    }
-
-    return term;
-}
-
+};
 
 
 double T(double x1) throw (ErrorA3, double) {
@@ -128,7 +102,7 @@ double T(double x1) throw (ErrorA3, double) {
         }
     }
     else {
-        throw ErrorA3("OpenFile");
+        throw ErrorA4("A4");
     }
 
 
@@ -198,11 +172,40 @@ double T(double x1) throw (ErrorA3, double) {
 
 }
 
-
 double U(double x1) throw (ErrorA3, double) {
     double term;
     string name;
 
+    ofstream file1("dat_X_1_1.dat");
+    if (file1.is_open()) {
+        string str = "-1,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1";
+        file1 << str << endl;
+        str = "-4.935,-3.013,-2.316,-1.819,-1.425,-1.097,-0.816,-0.571,-0.357,-0.167,0,0.147,0.276,0.386,0.477,0.548,0.597,0.617,0.597,0.505,0";
+        file1 << str << endl;
+        str = "1.935,0.464,1.327,1.976,2.502,2.951,3.344,3.695,4.013,4.303,4.571,4.618,4.645,4.652,4.636,4.596,4.524,4.412,4.240,3.956,0";
+        file1 << str << endl;
+    }
+    file1.close();
+    ofstream file2("dat_X_00_1.dat");
+    if (file2.is_open()) {
+        string str = "0,-0.05,-0.1,-0.15,-0.2,-0.25,-0.3,-0.35,-0.4,-0.45,-0.5,-0.55,-0.6,-0.65,-0.7,-0.75,-0.8,-0.85,-0.9,-0.95,-1";
+        file2 << str << endl;
+        str = "-4.935,-4.435,-3.936,-3.440,-2.948,-2.461,-1.98,-1.506,-1.041,-0.585,-0.141,0.292,0.712,1.117,1.507,1.882,2.239,2.578,2.898,3.199,3.48";
+        file2 << str << endl;
+        str = "1.935,1.835,1.735,1.636,1.537,1.44,1.344,1.249,1.156,1.065,0.976,0.889,0.806,0.724,0.646,0.572,0.5,0.432,0.368,0.308,0.252";
+        file2 << str << endl;
+    }
+    file2.close();
+    ofstream file3("dat_X1_00.dat");
+    if (file3.is_open()) {
+        string str = "0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1";
+        file3 << str << endl;
+        str = "-4.935,-2.663,-1.618,-0.773,-0.034,0.635,1.253,1.829,2.369,2.877,3.356,3.806,4.228,4.622,4.987,5.32,5.618,5.876,6.08,6.199,5.89";
+        file3 << str << endl;
+        str = "1.935,1.885,1.834,1.784,1.732,1.679,1.625,1.57,1.512,1.452,1.388,1.322,1.251,1.175,1.093,1.003,0.905,0.796,0.675,0.536,0.377";
+        file3 << str << endl;
+    }
+    file3.close();
     double ue = 0;
     if (x1 <= 1 && (x1 * -1) <= 1) {
         name = "dat_X_1_1.dat";
@@ -231,7 +234,7 @@ double U(double x1) throw (ErrorA3, double) {
         }
     }
     else {
-        throw ErrorA3("OpenFile");
+        throw ErrorA4("A4");
     }
 
     str = s[0];
@@ -351,62 +354,141 @@ double Srz(double x, double y, double z) throw (double) {
     }
     return term;
 }
-double Grs(double x, double y) throw (double) {
-    double term = 0;
-    term = 0.1389 * Srz(x + y, Gold(x, y), Glr(x, x * y)) + 1.8389 * Srz(x - y, Gold(y, x / 5), Glr(5 * x, x * y)) + 0.83 * Srz(x - 0.9, Glr(y, x / 5), Gold(5 * y, 5));
+
+
+double Srs(double x, double y, double z) throw (ErrorA2, ErrorA3, double) {
+    double term;
+
+    if (z > y && (z * z + x * y) > 0) {
+        term = Srz(x, y, z) + y * sqrt(z * z + x * y);
+    }
+    else if (z <= y && (x * x + z * y) > 0) {
+        term = y + Srz(z, x, y) * sqrt(x * x + z * y);
+    }
+    else if ((z * z + x * y) <= 0) {
+        throw ErrorA2("A2");
+    }
+    else if ((x * x + z * y) <= 0) {
+        throw ErrorA3("A3");
+    }
 
     return term;
-
+}
+double Qrz(double x, double y) throw (double) {
+    double term;
+    if (x < 1 && x>-1) {
+        term = x * Srs(x, y, x);
+    }
+    else {
+        term = y * Srs(y, x, y);
+    }
+    return term;
+}
+double Rrz(double x, double y, double z) throw (double) {
+    double term;
+    if (x > y) {
+        term = x * z * Qrz(y, z);
+    }
+    else {
+        term = y * x * Qrz(x, y);
+    }
+    return term;
+}
+double Grs(double x, double y, double z) throw (double) {
+    double term = 0;
+    term = 0.1389 * Rrz(x, y, y) + 1.8389 * Rrz(x - y, z, y);
+    return term;
 }
 
 double fun(double x, double y, double z) throw (double) {
     double temp = 0;
-
-    temp = x * x * Grs(y, z) + y * y * Grs(x, z) + 0.33 * x * y * Grs(x, z);
-
+    temp = x * Grs(x, y, z) + y * Grs(x, z, y);
     return temp;
 }
 
 
-
-double Gold1(double x, double y) throw (double) {
-    double term = 0;
-    if (x > y && (y > 0.1 || -y < 0.1)) {
-        term = x / y;
+double Srs1(double x, double y, double z) throw (ErrorA2, ErrorA4, double) {
+    double term;
+    if (z > y) {
+        term = Srz(x, y, z) + 1.44 * y * z;
     }
-    else if (y <= x && (x > 0.1 || -x < 0.1)) {
-        term = y / x;
+    else {
+        term = y + 1.44 * Srz(z, x, y);
     }
-    else if (x < y && (x > 0.1 || -x < 0.1)) {
-        term = 0.15;
-    }
-    else if (y == 0) {
-        term = 0.1;
-    }
-
     return term;
 }
-
-double Glr1(double x, double y) throw (double) {
-    double term = 0;
-    if (x < 1 || (x * -1) < 1) {
-        term = x;
+double Qrz1(double x, double y) throw (double) {
+    double term;
+    if (x < 1 && x>-1) {
+        term = x * Srs1(x, y, x);
     }
-    else if (x >= 1 || (x * -1) >= 1) {
-        term = y;
+    else {
+        term = y * Srs1(y, x, y);
     }
-
     return term;
 }
-double Grs1(double x, double y) throw (double) {
+double Rrz1(double x, double y, double z) throw (double) {
+    double term;
+    if (x > y) {
+        term = x * z * Qrz1(y, z);
+    }
+    else {
+        term = y * x * Qrz1(x, y);
+    }
+    return term;
+}
+double Grs1(double x, double y, double z) throw (double) {
     double term = 0;
-    term = 0.14 * Srz(x + y, Gold1(x, y), Glr1(x, x * y)) + 1.83 * Srz(x - y, Gold1(y, x / 5), Glr1(5 * x, x * y)) + 0.83 * Srz(x - 0.9, Glr1(y, x / 5), Gold1(5 * y, 5));
-
+    term = 0.1389 * Rrz1(x, y, y) + 1.8389 * Rrz1(x - y, z, y);
     return term;
 }
 double fun1(double x, double y, double z) throw (double) {
     double temp = 0;
-    temp = x * x * Grs1(y, z) + y * y * Grs1(x, z) + 0.33 * x * y * Grs1(x, z);
+
+    temp = x * Grs1(x, y, z) + y * Grs1(x, z, y);
+
+    return temp;
+}
+
+double Srs2(double x, double y, double z) throw (ErrorA2, ErrorA4, double) {
+    double term;
+    if (z > y) {
+        term = Srz(x, y, z) + y * x;
+    }
+    else {
+        term = y + z * Srz(z, x, y);
+    }
+    return term;
+}
+double Qrz2(double x, double y) throw (double) {
+    double term;
+    if (x < 1 && x>-1) {
+        term = x * Srs2(x, y, x);
+    }
+    else {
+        term = y * Srs2(y, x, y);
+    }
+    return term;
+}
+double Rrz2(double x, double y, double z) throw (double) {
+    double term;
+    if (x > y) {
+        term = x * z * Qrz2(y, z);
+    }
+    else {
+        term = y * x * Qrz2(x, y);
+    }
+    return term;
+}
+double Grs2(double x, double y, double z) throw (double) {
+    double term = 0;
+    term = 0.1389 * Rrz2(x, y, y) + 1.8389 * Rrz2(x - y, z, y);
+    return term;
+}
+double fun2(double x, double y, double z) throw (double) {
+    double temp = 0;
+
+    temp = x * Grs2(x, y, z) + y * Grs2(x, z, y);
 
     return temp;
 }
@@ -425,7 +507,11 @@ int main() {
     }
     catch (ErrorA3& e) {
         e.Message();
-        f = 1.3498 * z + 2.2362 * y - 2.348 * x * y;
+        f = fun2(x, y, z);
+    }
+    catch (ErrorA4& e) {
+        e.Message();
+        f = 1.3498 * x + 2.2362 * y * z - 2.348 * x * y;
     }
 
     cout << "\n fun =" << f << endl;
